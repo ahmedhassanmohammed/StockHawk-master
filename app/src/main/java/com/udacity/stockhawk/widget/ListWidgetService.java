8d437d8aef7ab502739e051a899a9ef669a99715
Ahmed Hassan <ahmedhassanmohammed@users.noreply.github.com>
2017-05-30 00:37:41 +0200

    test22

--- app/src/main/java/com/udacity/stockhawk/widget/ListWidgetService.java
+++ app/src/main/java/com/udacity/stockhawk/widget/ListWidgetService.java
@@ -9,7 +9,10 @@ import android.widget.RemoteViewsService;
  */
 
 public class ListWidgetService extends RemoteViewsService {
-
+    /*
+* call the Adapter of the listview
+*  Adapter is ListProvider
+* */
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
@@ -21,6 +24,79 @@ public class ListWidgetService extends RemoteViewsService {
     }
 
 }
+//    Cursor data =null;
+//    @Override
+//    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+//        return new RemoteViewsService.RemoteViewsFactory() {
+//            @Override
+//            public void onCreate() {
+//
+//
+//            }
+//
+//            @Override
+//            public void onDataSetChanged() {
+//
+//
+//                data = getContentResolver().query(Contract.Quote.URI,new String[]{Contract.Quote._ID, Contract.Quote.COLUMN_SYMBOL, Contract.Quote.COLUMN_PRICE,
+//                                Contract.Quote.COLUMN_PERCENTAGE_CHANGE, Contract.Quote.COLUMN_ABSOLUTE_CHANGE, Contract.Quote.COLUMN_HISTORY},
+//                        Contract.Quote.POSITION_SYMBOL + " = ?",
+//                        new String[]{"1"},
+//                        null);
+//
+//            }
+//
+//            @Override
+//            public void onDestroy() {
+//                if(data!=null)
+//                    data.close();
+//                data=null;
+//
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return 0;
+//            }
+//
+//            @Override
+//            public RemoteViews getViewAt(int i) {
+//                if (i == AdapterView.INVALID_POSITION ||
+//                        data == null || !data.moveToPosition(i)) {
+//                    return null;
+//                }
+//
+//                RemoteViews views =new RemoteViews(getPackageName(),R.layout.list_item_quote);
+//                String symbol = data.getString(data.getColumnIndex("symbol"));
+//
+//                views.setTextViewText(R.id.symbol, symbol);
+//                views.setTextViewText(R.id.price, data.getString(data.getColumnIndex("price")));
+//                views.setTextViewText(R.id.change, data.getString(data.getColumnIndex("percentage_change")));
+//
+//                return views;
+//            }
+//
+//            @Override
+//            public RemoteViews getLoadingView() {
+//                return null;
+//            }
+//
+//            @Override
+//            public int getViewTypeCount() {
+//                return 1;
+//            }
+//
+//            @Override
+//            public long getItemId(int position ) {
+//                return position;
+//            }
+//
+//            @Override
+//            public boolean hasStableIds() {
+//                return true;
+//            }
+//        };
+//    }
 
 
 
